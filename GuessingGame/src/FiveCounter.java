0715f666cf221747a2248edc5586136c73cb431a
shrey <shrey@shreys-air>
2017-10-09 18:43:54 +0530

    First commit

---
+++ GuessingGame/src/FiveCounter.java
@@ -0,0 +1,17 @@
+
+public class FiveCounter {
+
+	public static void main(String[] args) {
+		counter();
+	}
+    
+	public static void counter()
+	{
+		 int i=5;
+		for(i=1;i<5;i++) {
+			
+		  System.out.println("i++") ;
+	        	
+		}
+	}
+}
---
+++ GuessingGame/src/Hello.java
@@ -0,0 +1,22 @@
+
+
+public class Hello {
+	
+	public static void main(String[]args) {
+      //public static just a keyword, void return type method means shows there
+	 //return method to it ,main is  used to call method 
+		System.out.println("Method Example");
+	     
+		sayHello();
+		  //calling the method(sayHello)
+}
+	public static void sayHello() 
+	{
+		System.out.println("Hello World");
+		
+		  
+	}
+	
+		
+	
+}
